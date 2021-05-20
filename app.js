5b998f0d81056f8a10348e406bc662a10922977e
Matthew Mao <matty8100@gmail.com>
2021-05-19 22:50:51 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Configuration
+require('dotenv').config()
+const { CommandoClient } = require('discord.js-commando');
+const path = require('path');
+
+const client = new CommandoClient({
+	commandPrefix: '&',
+	owner: '115661304831803393'
+});
+
+client.registry
+	.registerDefaultTypes()
+	.registerGroups([])
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+	.registerCommandsIn(path.join(__dirname, 'commands'));
+
+  client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+    client.user.setActivity('with Commando');
+  });
+  
+  client.on('error', console.error);
+
+  client.login(process.env.BOT_TOKEN);
