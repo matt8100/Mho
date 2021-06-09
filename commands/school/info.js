741164a05b94001046a2ac3e7897bb9e8e62c385
Matthew Mao <matty8100@gmail.com>
2021-06-08 19:04:07 -0700

    add sessional date lookup

--- commands/school/info.js
+++ commands/school/info.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class Info extends Command {
   constructor(client) {
@@ -26,25 +27,32 @@ module.exports = class Info extends Command {
   run(message, arg) {
     const { client } = this;
 
+    function constructEmbed(value) {
+      const embed = new MessageEmbed()
+        .setColor('#162951')
+        .setDescription(value);
+      return embed;
+    }
+
+    // For listing all existing keys
     if (arg.key === 'list') {
       const stmt = client.db.prepare('SELECT key FROM INFO');
       client.db.transaction(() => {
         try {
-          const keys = stmt.all();
+          const keys = stmt.all(); // Retrieves array of single-element JSONs
           for (let i = 0; i < keys.length; i += 1) keys[i] = keys[i].key;
-          message.say(keys.sort().join(', '));
+          return message.say(keys.sort().join(', '));
         } catch (err) {
           message.react('❌');
         }
       })();
-    } else {
+    } else { // Standard run
       const stmt = client.db.prepare(`SELECT * FROM info WHERE key = lower('${arg.key}')`);
       client.db.transaction(() => {
         try {
           const info = stmt.get();
-          if (info) {
-            message.say(info.value);
-          } else message.react('❌');
+          if (info) return message.say(constructEmbed(info.value));
+          message.react('❌');
         } catch (err) {
           message.react('❌');
         }
