c5739b670bf04f301894c9e5375c29a8f606e46a
Matthew Mao <matty8100@gmail.com>
2021-06-16 23:14:39 -0700

    add server-independence to SQL queries

--- commands/school/infoedit.js
+++ commands/school/infoedit.js
@@ -8,7 +8,7 @@ module.exports = class InfoEdit extends Command {
       group: 'school',
       memberName: 'infoedit',
       description: 'Editing info for the info command',
-      guildOnly: false,
+      guildOnly: true,
       userPermissions: ['MANAGE_MESSAGES'],
       throttling: {
         usages: 2,
@@ -19,18 +19,20 @@ module.exports = class InfoEdit extends Command {
           key: 'option',
           prompt: 'Do you want to `add`, `edit`, or `delete` info?',
           type: 'string',
-          oneOf: ['add', 'a', 'edit', 'e', 'delete', 'del', 'd'],
+          oneOf: ['add', 'a', 'edit', 'e', 'delete', 'del', 'd', 'rm'],
         },
         {
           key: 'key',
           prompt: 'What is the key of the info you want to edit?',
           type: 'string',
+          validate: (text) => { if (text.length <= 20) return true; },
         },
         {
           key: 'value',
           prompt: 'What is the info you want to add/edit? If you are deleting an entry, type the name once more to confirm.',
           type: 'string',
           default: '',
+          validate: (text) => { if (text.length <= 1000) return true; },
         },
       ],
     });
@@ -38,30 +40,32 @@ module.exports = class InfoEdit extends Command {
 
   run(message, arg) {
     const { client } = this;
+    const guild = message.guild.id;
 
     function add() {
-      if (!arg.value) return message.reply('No text specified!');
+      if (!arg.value) return message.say('No text specified!');
       if (arg.key === 'list') return message.react('❌');
-      const stmt = client.db.prepare(`INSERT INTO info (key, value) VALUES (lower('${arg.key}'), '${arg.value}')`);
+      const stmt = client.db.prepare(`INSERT INTO info (guild, key, value) VALUES (${guild}, lower('${arg.key}'), '${arg.value}')`);
       client.db.transaction(() => {
         try {
           stmt.run();
           message.react('✅');
         } catch (err) {
-          if (err.message === 'UNIQUE constraint failed: info.key') return message.reply('Entry already exists!');
+          console.log(err.message);
+          if (err.message === 'Entry already exists!') return message.say(err.message);
           message.react('❌');
         }
       })();
     }
 
     function edit() {
-      if (!arg.value) return message.reply('No text specified!');
-      const stmt = client.db.prepare(`UPDATE info SET value = '${arg.value}' WHERE key = lower('${arg.key}')`);
+      if (!arg.value) return message.say('No text specified!');
+      const stmt = client.db.prepare(`UPDATE info SET value = '${arg.value}' WHERE guild = '${guild}' AND key = lower('${arg.key}')`);
       client.db.transaction(() => {
         try {
           const info = stmt.run();
           if (info.changes) message.react('✅');
-          else message.reply('No such entry!');
+          else message.say('No such entry!');
         } catch (err) {
           message.react('❌');
         }
@@ -69,12 +73,12 @@ module.exports = class InfoEdit extends Command {
     }
 
     function del() {
-      const stmt = client.db.prepare(`DELETE FROM info WHERE key = lower('${arg.key}')`);
+      const stmt = client.db.prepare(`DELETE FROM info WHERE guild = '${guild}' AND key = lower('${arg.key}')`);
       client.db.transaction(() => {
         try {
           const info = stmt.run();
           if (info.changes) message.react('✅');
-          else message.reply('No such entry!');
+          else message.say('No such entry!');
         } catch (err) {
           message.react('❌');
         }
@@ -93,6 +97,7 @@ module.exports = class InfoEdit extends Command {
       case 'delete':
       case 'del':
       case 'd':
+      case 'rm':
         del();
         return message;
       default:
