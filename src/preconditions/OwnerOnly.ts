588fb3d0387116a38c9d24fffe976e1fecd827c1
Matthew Mao <matty8100@gmail.com>
2021-07-15 21:46:57 -0700

    add OwnerOnly precondition

---
+++ src/preconditions/OwnerOnly.ts
@@ -0,0 +1,12 @@
+import { AsyncPreconditionResult, Precondition } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+
+const owners = ['115661304831803393'];
+
+export default class UserPrecondition extends Precondition {
+  public async run(message: Message): AsyncPreconditionResult {
+    return owners!.includes(message.author.id) ? this.ok() : this.error({
+      context: { silent: true },
+    });
+  }
+}
