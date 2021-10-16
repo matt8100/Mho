0ab4387848268473d16c88b13f47546c0fd6e957
Matthew Mao <matty8100@gmail.com>
2021-10-16 16:20:10 -0400

    remove textual commands in favor of slash commands

--- src/commands/util/ping.ts
+++ src/commands/util/ping.ts
@@ -8,7 +8,7 @@ import { Message } from 'discord.js';
 })
 
 export default class extends Command {
-  public async run(message: Message): Promise<Message> {
+  public async messageRun(message: Message): Promise<Message> {
     const msg = await message.channel.send('Ping?');
     return msg.edit(`Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${msg.createdTimestamp - message.createdTimestamp}ms.`);
   }
